1b35973a48bc2b8d5468b9741b4254caf71397f3
Sebastian Heinz <sheinz@fh-Bielefeld.de>
2012-06-14 14:50:33 +0200

    Gui startseite eingefügt

--- src/aufgabe7/gui/Gui.java
+++ src/aufgabe7/gui/Gui.java
@@ -7,19 +7,61 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 public class Gui extends JFrame implements ActionListener, MouseListener{
-private JFrame f;
-	
+private JFrame start;
+private JPanel panel1;	
+private JMenuBar menu;
+private JMenu hp,hilfe;
+private JMenuItem oeffnen,beenden,help;
+
 	public Gui(String name) {
 		
 		
-		f = new JFrame(name);
-		f.addMouseListener(this);
-		f.setLayout(new GridLayout(3,1));
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(1000, 600);
-		f.setVisible(true);
+		start = new JFrame(name);
+		start.addMouseListener(this);
+		start.setLayout(new GridLayout(3,1));
+		start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		start.setSize(400, 400);
+		panel1 = new JPanel();
+		menu();
+		start.setJMenuBar(menu);
+		start.setVisible(true);
+	}
+	
+	
+public void menu(){
+		
+		// Menüleiste erzeugen
+		menu = new JMenuBar();
+		menu.setSize(getWidth(), 10);
+		menu.setAlignmentX(LEFT_ALIGNMENT);
+		
+		// Menüpunkte erzeugen
+		
+		hp = new JMenu("Hauptmenue");
+		hilfe = new JMenu("Help");
+		
+		// Untermenüelemente erzeugen
+        oeffnen = new JMenuItem("Neustart");
+        oeffnen.addActionListener(this);
+        beenden = new JMenuItem("Beenden");
+        beenden.addActionListener(this);
+        help = new JMenuItem("hilfe");
+        help.addActionListener(this);
+        
+     // Menüelemente hinzufügen
+        menu.add(hp);
+        menu.add(hilfe);
+        
+     // Untermenüelemente hinzufügen
+        hp.add(oeffnen);
+        hp.add(beenden);
+     
 	}
 	/**
 	 * @param args
