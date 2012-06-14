fb9fe438873ca7c848e295f79395d23eba14a83a
Sebastian Heinz <sheinz@fh-Bielefeld.de>
2012-06-14 14:21:07 +0200

    Player.java eingefügt

---
+++ src/aufgabe7/player/Player.java
@@ -0,0 +1,16 @@
+package aufgabe7.player;
+
+public class Player 
+{
+
+	private int x,y,punkte;
+	
+	
+	public Player(int tempx,int tempy)
+	{
+		this.x=tempx;
+		this.y=tempy;
+		this.punkte=0;
+		
+	}
+}
