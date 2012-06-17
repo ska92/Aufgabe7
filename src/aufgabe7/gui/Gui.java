f8a0d930cd02478b2a4815dd308ecd88e35e698b
Sebastian Heinz <sheinz@fh-Bielefeld.de>
2012-06-17 18:14:56 +0200

    weiter an der Gui gearbeitet

--- src/aufgabe7/gui/Gui.java
+++ src/aufgabe7/gui/Gui.java
@@ -1,16 +1,20 @@
 package aufgabe7.gui;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Gui extends JFrame implements ActionListener, MouseListener{
 private JFrame start;
@@ -21,13 +25,34 @@ private JMenuItem oeffnen,beenden,help;
 
 	public Gui(String name) {
 		
+		JLabel beschriftung_Sp1 = new JLabel( "Spieler 1: " );
+		JLabel beschriftung_Sp2 = new JLabel( "Spieler 2: " );
+		JTextField textfeld_Sp1 = new JTextField();
+		JTextField textfeld_Sp2 = new JTextField();
+		JComboBox combo_Sp1 = new JComboBox();
+		JComboBox combo_Sp2 = new JComboBox();
+		JPanel jp1 = new JPanel();
+		JPanel jp2 = new JPanel();
 		
 		start = new JFrame(name);
 		start.addMouseListener(this);
-		start.setLayout(new GridLayout(3,1));
+		start.setLayout(new GridLayout(4,1));
 		start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		start.setSize(400, 400);
-		panel1 = new JPanel();
+		
+		jp1.setLayout(new GridLayout(1,3));
+		jp1.setSize(400, 20);
+		jp1.add(beschriftung_Sp1);
+		jp1.add(textfeld_Sp1);
+		jp1.add(combo_Sp1);
+		
+		jp2.setLayout(new GridLayout(1,3));
+		jp2.add(beschriftung_Sp2);
+		jp2.add(textfeld_Sp2);
+		jp2.add(combo_Sp2);
+		
+		start.add(jp1);
+		start.add(jp2);
 		menu();
 		start.setJMenuBar(menu);
 		start.setVisible(true);
